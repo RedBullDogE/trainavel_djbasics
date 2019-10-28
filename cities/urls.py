6d984196de280f4512d89e150185197cfa1c40f1
Drew <gsb06092000@gmail.com>
2019-10-28 23:42:31 +0200

    Add details view for each city

--- cities/urls.py
+++ cities/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('detail/<int:pk>/', views.CityDetailView.as_view(), name='detail'),
     path('', views.home, name='home')
 ]

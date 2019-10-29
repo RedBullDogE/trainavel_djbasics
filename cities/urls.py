8cd59076dbe07ebb023b10ace9f6fa7596b16737
Drew <gsb06092000@gmail.com>
2019-10-30 00:02:34 +0200

    Add POST form to cities home page; add create.html for adding new cities

--- cities/urls.py
+++ cities/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('detail/<int:pk>/', views.CityDetailView.as_view(), name='detail'),
+    path('add/', views.CityCreateView.as_view(), name='add'),
     path('', views.home, name='home')
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import City
+from .forms import CityForm
+
 
 def home(request):
-    cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities})
+    if request.method == 'POST':
+        form = CityForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+    form = CityForm()
+    cities = City.objects.all()     # pylint: disable=maybe-no-member
+    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
 
 
 class CityDetailView(DetailView):
-    queryset = City.objects.all()
+    queryset = City.objects.all()    # pylint: disable=maybe-no-member
     context_object_name = 'object'
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('city:home')

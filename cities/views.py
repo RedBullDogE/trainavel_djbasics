588b09bc3564c5038935864429e8e3fd54743ff9
Drew <gsb06092000@gmail.com>
2019-10-30 19:27:34 +0200

    Add delete and udpate functions to home page and corresponging views. Add table to home page with cities list. Add pagination

--- cities/views.py
+++ cities/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from .models import City
 from .forms import CityForm
 
 
 def home(request):
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-    form = CityForm()
-    cities = City.objects.all()     # pylint: disable=maybe-no-member
-    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+    city_list = City.objects.all()     # pylint: disable=maybe-no-member
+    paginator = Paginator(city_list, 2)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    return render(request, 'cities/home.html', {'objects_list': cities})
 
 
 class CityDetailView(DetailView):
@@ -26,4 +25,18 @@ class CityCreateView(CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
+    success_url = reverse_lazy('city:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('city:home')
+
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
     success_url = reverse_lazy('city:home')

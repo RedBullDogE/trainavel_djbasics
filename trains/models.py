82a6454602fe0139b123dc88ae734cd9c1c73cd2
Drew <gsb06092000@gmail.com>
2019-11-01 14:57:23 +0200

    Initial app settings: creating model, registering in admin panel, making migrations

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cities.models import City
 
-# Create your models here.
+
+class Train(models.Model):
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name='Номер поезда')
+    from_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name='Откуда', related_name='from_city')
+    to_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name='Куда', related_name='to_city')
+    travel_time = models.IntegerField(verbose_name='Время в пути')
+    
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['name']
+    
+    def __str__(self):
+        return f"Поезд №{self.name} из {self.from_city} в {self.to_city}"

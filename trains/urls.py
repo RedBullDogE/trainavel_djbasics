29de28c93c61c9b6d4efdca4fc998a750e205f46
Drew <gsb06092000@gmail.com>
2019-11-02 23:58:22 +0200

    Add detail, home and update views and templates

--- trains/urls.py
+++ trains/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('detail/<int:pk>/', views.CityDetailView.as_view(), name='detail'),
-    # path('update/<int:pk>/', views.CityUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>/', views.CityDeleteView.as_view(), name='delete'),
+    path('detail/<int:pk>/', views.TrainDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', views.TrainUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.TrainDeleteView.as_view(), name='delete'),
     path('add/', views.TrainCreateView.as_view(), name='add'),
     path('', views.home, name='home')
 ]
--- trains/views.py
+++ trains/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 from .models import Train
 from .forms import TrainForm
+
 
 def home(request):
     train_list = Train.objects.all()     # pylint: disable=maybe-no-member
@@ -14,9 +17,32 @@ def home(request):
     return render(request, 'trains/home.html', {'objects_list': trains})
 
 
+class TrainDetailView(DetailView):
+    queryset = Train.objects.all()    # pylint: disable=maybe-no-member
+    context_object_name = 'object'
+    template_name = 'trains/detail.html'
+
+
 class TrainCreateView(SuccessMessageMixin, CreateView):
     model = Train
     form_class = TrainForm
     template_name = 'trains/create.html'
     success_url = reverse_lazy('train:home')
     success_message = 'Поезд успешно добавлен!'
+
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('train:home')
+    success_message = 'Данные о поезде успешно отредактированы!'
+
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    success_url = reverse_lazy('train:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Поезд успешно удалён')
+        return self.post(request, *args, **kwargs)

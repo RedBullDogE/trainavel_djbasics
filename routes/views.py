9b442e54473968d58c0faf281182e78e57329067
Drew <gsb06092000@gmail.com>
2019-11-15 00:45:16 +0200

    Add displaying found routes

--- routes/views.py
+++ routes/views.py
@@ -48,40 +48,59 @@ def find_routes(request):
             to_city = data['to_city']
             across_cities_form = data['across_cities']
             travel_time = data['travel_time']
+
             graph = get_graph()
-            all_ways = list(dfs_paths(graph, from_city.id, to_city.id))
-            if not all_ways:
+            all_ways_list = list(dfs_paths(graph, from_city.id, to_city.id))
+
+            if not all_ways_list:
                 messages.error(
                     request, 'Маршрута, удовлетворяющего условиям, не существует')
                 return render(request, 'routes/home.html', {'form': form})
+
             if across_cities_form:
                 across_cities = [city.id for city in across_cities_form]
-                right_ways = []
-                for way in all_ways:
+                ways_with_cities_list = []
+                for way in all_ways_list:
                     if all(point in way for point in across_cities):
-                        right_ways.append(way)
-                if not right_ways:
+                        ways_with_cities_list.append(way)
+                if not ways_with_cities_list:
                     messages.error(
                         request, 'Маршрут через заданные города невозможен')
                     return render(request, 'routes/home.html', {'form': form})
             else:
-                right_ways = all_ways
+                ways_with_cities_list = all_ways_list
 
-        trains = [
-            [Train.objects.filter(
-                from_city=way[i],
-                to_city=way[i+1]
-            ).order_by('travel_time').first() for i in range(len(way) - 1)]
-            for way in right_ways]
-        travel_time = [sum([train.travel_time for train in way]) for way in trains]
-        # --- test ---
-        cities = City.objects.all()
-        for way in right_ways:
-            for i in range(len(way)):
-                way[i] = City.objects.get(id=way[i]).name
+            trains_list = [
+                [Train.objects.filter(
+                    from_city=way[i],
+                    to_city=way[i+1]
+                ).order_by('travel_time').first() for i in range(len(way) - 1)]
+                for way in ways_with_cities_list
+            ]
 
-        context = {'form': RouteForm, 'ways': right_ways}
-        return render(request, 'routes/home.html', context)
+            routes = []
+            for trains in trains_list:
+                routes.append({
+                    'route': trains,
+                    'total_time': sum([train.travel_time for train in trains]),
+                    'from_city': from_city,
+                    'to_city': to_city
+                })
+            routes_with_suitable_time = list(
+                filter(lambda x: x['total_time'] <= int(travel_time), routes))
+
+            if not routes_with_suitable_time:
+                messages.error(
+                    request, 'Время в пути найденных маршрутов больше заданого.')
+                return render(request, 'routes/home.html', {'form': form})
+
+            routes_with_suitable_time.sort(key=lambda x: x['total_time'])
+            context = {
+                'form': RouteForm,
+                'routes': routes_with_suitable_time,
+                'from_city': from_city,
+                'to_city': to_city}
+            return render(request, 'routes/home.html', context)
     else:
         messages.error(request, 'Создайте маршрут')
         form = RouteForm()

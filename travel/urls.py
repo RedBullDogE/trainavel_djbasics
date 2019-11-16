e2843a31531f1a5b0e7f7449f7f775a233b6bc99
Drew <gsb06092000@gmail.com>
2019-11-16 23:43:36 +0200

    detail view

--- travel/urls.py
+++ travel/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from routes.views import (home, find_routes, add_route,
-                          RouteListView)
+                          RouteListView, RouteDetailView, RouteDeleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +25,7 @@ urlpatterns = [
     path('find/', find_routes, name='find_routes'),
     path('add_route/', add_route, name='add_route'),
     path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
+    path('/<int:pk>/', RouteDeleteView.as_view(), name='delete'),
     path('', home, name='home')
 ]
